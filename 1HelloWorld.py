d118f41735fcc803544319bb74030242e629f62a
Theo <theodoregerst@students.berkeley.net>
2020-12-15 19:52:23 -0800

    Diamond code (adjust the width variable)

--- 1HelloWorld.py
+++ 1HelloWorld.py
@@ -1,1 +1,18 @@
-print('hello world')
+width = 30
+print('.')
+for i in range(width):
+    returning = '.'
+    for j in range(i):
+        returning += ' '
+    returning += '.'
+    print(returning)
+for i in range(width + 2):
+    returning = ''
+    for j in range(i):
+        returning += ' '
+    returning += '.'
+    for j in range(width - i):
+       returning += ' '
+    if(i != width + 1):
+        returning += '.'
+    print(returning)
